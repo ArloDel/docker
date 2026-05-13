a370e4f904e404a24c12c79cec65f1f820373aca
arlodel <nurrahman1505@gmail.com>
2026-05-13 09:47:08 +0700

    Add backup and restore scripts for Docker volumes

--- docker-volume-bakup.sh
+++ docker-volume-bakup.sh
@@ -6,3 +6,15 @@ docker container create --name nginxbackup --mount "type=bind,source=D:\COURSE\d
 docker container start nginxbackup
 
 docker container exec -i -t nginxbackup /bin/bash
+
+tar cvf /backup/backup.tar.gz /data
+
+docker container stop nginxbackup
+
+docker container rm nginxbackup
+
+docker container start mongovolume
+
+docker container stop mongovolume
+
+docker container run --rm --name ubuntubackup --mount "type=bind,source=D:\COURSE\docker\backup,destination=/backup" --mount "type=volume,source=mongo-volume,destination=/data" ubuntu:latest tar cvf /backup/backup1.tar.gz /data
---
+++ docker-volume-restore.sh
@@ -0,0 +1,6 @@
+docker volume create mongorestore
+
+docker container run --rm --name ubunturestore --mount "type=bind,source=D:\COURSE\docker\backup,destination=/backup" --mount "type=volume,source=mongorestore,destination=/data" ubuntu:latest bash -c "cd /data && tar xvf /backup/backup.tar.gz --strip 1"
+
+
+docker container create --name mongorestore --mount "type=volume,source=mongorestore,target=/data/db" --publish 27020:27017 --env MONGO_INITDB_ROOT_USERNAME=admin --env MONGO_INITDB_ROOT_PASSWORD=admin mongo:latest
