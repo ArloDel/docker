8d7b239ffa2cbd5c3de82dba51abe87c98839147
arlodel <nurrahman1505@gmail.com>
2026-06-03 12:29:56 +0700

    Add docker inspect script for managing images, networks, volumes, and containers

---
+++ docker-inspect.sh
@@ -0,0 +1,14 @@
+# docker image inspect namaimage
+
+docker image inspect mongo-express:latest
+
+# docker network inspect nama-network
+docker network inspect mongonetwork
+
+# docker volume inspect nama-volume
+
+docker volume inspect mongorestore
+
+# docker container inspect nama-container
+
+docker container inspect mongodb
