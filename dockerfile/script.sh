78c0c9a7694a688be63bdce1ec621fe39ba5659f
arlodel <nurrahman1505@gmail.com>
2026-06-05 10:05:30 +0700

    Add Dockerfile for basic image setup and update script for building images

--- dockerfile/script.sh
+++ dockerfile/script.sh
@@ -1,4 +1,9 @@
+# FROM instruction is used to specify the base image for the Docker image being built.
 docker build -t arlodel/from from
-
-
 docker image ls
+
+
+# RUN instruction is used to execute commands in the Docker image during the build process.
+docker build -t arlodel/run run
+docker image ls
+docker build -t arlodel/run run --progress=plain --no-cache
